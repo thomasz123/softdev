fa17185e46529b0942dd776bc3beeb00f15c23d1
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2022-09-21 22:25:08 -0400

    Update list2.py

---
+++ 03_py/list2/list2.py
@@ -0,0 +1,101 @@
+# Return the number of even ints in the given array. Note: the % "mod" operator
+# computes the remainder, e.g. 5 % 2 is 1.
+
+def count_evens(nums):
+  count = 0
+  for x in nums:
+    if (x % 2 == 0):
+      count += 1
+  return count
+
+print(count_evens([2, 1, 2, 3, 4])) # 3
+print(count_evens([2, 2, 0])) # 3
+print(count_evens([1, 3, 5])) # 0
+
+
+# Given an array length 1 or more of ints, return the difference between the
+# largest and smallest values in the array. Note: the built-in min(v1, v2) and
+# max(v1, v2) functions return the smaller or larger of two values.
+
+def big_diff(nums):
+  return max(nums) - min(nums)
+
+print(big_diff([10, 3, 5, 6])) # 7
+print(big_diff([7, 2, 10, 9])) # 8
+print(big_diff([2, 10, 7, 2])) # 8
+
+
+# Return the "centered" average of an array of ints, which we'll say is the mean
+# average of the values, except ignoring the largest and smallest values in the
+# array. If there are multiple copies of the smallest value, ignore just one copy,
+# and likewise for the largest value. Use int division to produce the final
+# average. You may assume that the array is length 3 or more.
+
+def centered_average(nums):
+  nums.remove(min(nums))
+  nums.remove(max(nums))
+  sum = 0
+  for x in nums:
+    sum += x
+  average = sum / len(nums)
+  return average
+
+print(centered_average([1, 2, 3, 4, 100])) # 3
+print(centered_average([1, 1, 5, 5, 10, 8, 7])) # 5
+print(centered_average([-10, -4, -2, -4, -2, 0])) # -3
+
+
+# Return the sum of the numbers in the array, returning 0 for an empty array.
+# Except the number 13 is very unlucky, so it does not count and numbers that
+# come immediately after a 13 also do not count.
+
+def sum13(nums):
+  sum = 0
+  i = 0
+  while i < len(nums):
+    if (nums[i] == 13):
+      i += 2
+    else:
+      sum += nums[i]
+      i += 1
+  return sum
+
+print(sum13([1, 2, 2, 1])) # 6
+print(sum13([1, 1])) # 2
+print(sum13([1, 2, 2, 1, 13])) # 6
+
+
+# Return the sum of the numbers in the array, except ignore sections of numbers
+# starting with a 6 and extending to the next 7 (every 6 will be followed by at
+# least one 7). Return 0 for no numbers.
+
+def sum67(nums):
+  sum = 0
+  i = 0
+  while i < len(nums):
+    if (nums[i] == 6):
+      while (nums[i] != 7):
+        i += 1
+    else:
+      sum += nums[i]
+    i += 1
+  return sum
+
+print(sum67([1, 2, 2])) # 5
+print(sum67([1, 2, 2, 6, 99, 99, 7])) # 5
+print(sum67([1, 1, 6, 7, 2])) # 4
+
+
+# Given an array of ints, return True if the array contains a 2 next to a 2 somewhere.
+
+def has22(nums):
+  i = 0
+  while i < len(nums) - 1:
+    if (nums[i] == 2) and (nums[i + 1] == 2):
+      return True
+    i += 1
+  return False
+
+print(has22([1, 2, 2])) # True
+print(has22([1, 2, 1, 2])) # False
+print(has22([2, 1, 2])) # False
