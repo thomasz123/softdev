5a10bb5be3708ebff3e91f2b56bc4e7225d1121b
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2022-09-28 22:59:38 -0400

    Update krewes.py

---
+++ 04_choose/krewes.py
@@ -0,0 +1,36 @@
+# Thomas Zhang, Nicole Zhou, Bob, Duck
+# SoftDev
+# K04 -- Krewes
+# 2022-09-23
+# time spent: 1.5 hrs
+
+'''
+DISCO:
+1) .keys() access the keys in an dictionary
+2) .choice() access a random element in an iterable list
+
+QCC:
+1) Is this really random? Is .choice() more biased than randint()?
+
+OPS SUMMARY:
+1) Access the list of keys in a dictionary.
+2) We then access a random key in the list of keys.
+3) After that, we use .choice() to access a random element in the value of the chosen key.
+4) Return the element.
+'''
+
+import random as ran
+
+def devo(diction):
+    keys = list(diction.keys())
+    rand_key = ran.choice(keys)
+    rand_val = ran.choice(diction.get(rand_key))
+    return rand_val
+
+krewes = {
+           2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"], 
+           7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+           8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+         }
+
+print(devo(krewes))
